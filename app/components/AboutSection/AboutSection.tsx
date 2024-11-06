61d3dc19fbf8c8e443021938ead5b2049a212b66
tloyan <thomas.loyan@gmail.com>
2024-11-06 10:22:32 +0100

    add about section

---
+++ app/components/AboutSection/AboutSection.tsx
@@ -0,0 +1,9 @@
+export function AboutSection() {
+  return (
+    <section className="border-t-[.1rem] py-[5.6rem] sm:py-[12.6rem] font-grotesk text-[1.4rem] text-center sm:text-[1.8rem] sm:text-start space-y-[1.2rem] sm:space-y-[2.4rem]">
+      <p>Hello  from an eternal pationated learner. I’am enjoying with nearly everything, I can play with  embarked system then a website  then a design then an algorythm. But even with that there is things always present in my mind... doing things work, doing things right, doing thing fast and optimised. Also I love taking in consideration that things are made for a good reason, for people. I think taking Tech things from business perspective is a essential.Currently I am playing with frontendmentor challenge, also learning algo things and reading about clean architecture and more</p>
+      <p>If you want to know me more. I love my familly and I love taking time with my child, discovering life with him a travers this eyes. Like I said I couldnt say that I have one passion because I get passion from everything easily but if you want some, I love learning and trying new thing like cooking, gardening, I love board game and role game like Dungeon dragon, cycling and walk my dog. I also pationated by all these different culture and people in this world.</p>
+      <p>Be warned ! I probably begin to be pationated by your project </p>
+    </section>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "./components/HeroSection/HeroSection"
 import { Header } from "./components/Header/Header"
 import { WorkWithSection } from "./components/WorkWithSection/WorkWithSection"
 import { StackSection } from "./components/StackSection/StackSection"
+import { AboutSection } from "./components/AboutSection/AboutSection"
 
 export default function Page() {
   return (
@@ -11,6 +12,7 @@ export default function Page() {
       <main className="px-[1.6rem] sm:px-[3.2rem] lg:px-[16.4rem]">
         <WorkWithSection />
         <StackSection />
+        <AboutSection />
       </main>
     </div>
   )
